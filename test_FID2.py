9464420d6de7d84c3adaf4acb689b5899392fa69
honggkang <rednine13@gmail.com>
2023-11-15 17:03:11 +0900

    update

--- test_FID2.py
+++ test_FID2.py
@@ -21,7 +21,7 @@ parser.add_argument("--bs", type=int, default=100, help="size of the batches")
 ### GAN
 parser.add_argument('--latent_dim', type=int, default=100)
 ### VAE parameters
-parser.add_argument("--latent_size", type=int, default=20, help="dimensionality of the latent space")
+parser.add_argument("--latent_size", type=int, default=20, help="dimensionality of the latent space") # VAE
 ### DDPM parameters
 parser.add_argument('--n_feat', type=int, default=128)
 parser.add_argument('--n_T', type=int, default=100)
@@ -29,8 +29,8 @@ parser.add_argument('--guide_w', type=float, default=0)  # 0, 0.5, 2
 ### CUDA
 parser.add_argument('--device_id', type=str, default='3')
 ### Parameters for FID
-parser.add_argument('--gen', type=str, default='dcgan')  # gan(GAN), vae(VAE), ddpm(DDPM)
-parser.add_argument('--gen_dir', type=str, default='checkpoint/FedDCGANupdateGEN2.pt')
+parser.add_argument('--gen', type=str, default='cvae')  # gan(GAN), vae(VAE), ddpm(DDPM)
+parser.add_argument('--gen_dir', type=str, default='checkpoint/FedCVAEfreezeGEN1.pt')
 parser.add_argument('--img_dir', type=str, default='imgs/')
 
 args = parser.parse_args()
@@ -40,27 +40,27 @@ args.img_shape = (args.output_channel, args.img_size, args.img_size)
 tf = transforms.Compose([
     transforms.ToTensor(),
     transforms.Resize(args.img_size),
-    transforms.Normalize([0.5], [0.5])
+    # transforms.Normalize([0.5], [0.5])
 ])  # mnist is already normalised 0 to 1
 
 train_data = datasets.MNIST(root='/home/hong/NeFL/.data/mnist', train=True, transform=tf, download=True)
 test_data = datasets.MNIST(root='/home/hong/NeFL/.data/mnist', train=False, transform=tf, download=True)
 
 # Check if the provided generator value is in the list of valid generators
-valid_generators = ['gan', 'GAN', 'vae', 'VAE', 'ddpm', 'DDPM', 'dcgan', 'DCGAN']
+valid_generators = ['gan', 'GAN', 'vae', 'VAE', 'ddpm', 'DDPM', 'dcgan', 'DCGAN', 'cvae', 'CVAE']
 if args.gen.lower() not in valid_generators:
     print("Warning: The provided --generator value is not one of 'gan', 'vae', or 'ddpm'.")
     print("Please provide a valid generator value.")
 if not os.path.exists(args.gen_dir):
     print("Please provide a valid generator model path.")
 
-bs = 2
-cuda = True if torch.cuda.is_available() else False
-FloatTensor = torch.cuda.FloatTensor if cuda else torch.FloatTensor
-LongTensor = torch.cuda.LongTensor if cuda else torch.LongTensor
-z = Variable(FloatTensor(np.random.normal(0, 1, (bs, args.latent_dim))))  # generator
-x = torch.zeros(bs, 1, 28, 28, device=args.device)  # discriminator , VAE, DDPM
-c = Variable(LongTensor(np.random.randint(0, args.num_classes, bs)))
+# bs = 2
+# cuda = True if torch.cuda.is_available() else False
+# FloatTensor = torch.cuda.FloatTensor if cuda else torch.FloatTensor
+# LongTensor = torch.cuda.LongTensor if cuda else torch.LongTensor
+# z = Variable(FloatTensor(np.random.normal(0, 1, (bs, args.latent_dim))))  # generator
+# x = torch.zeros(bs, 1, 28, 28, device=args.device)  # discriminator , VAE, DDPM
+# c = Variable(LongTensor(np.random.randint(0, args.num_classes, bs)))
 
 
 ######### source image (train)#########
@@ -185,6 +185,38 @@ elif args.gen == "vae" or args.gen == "VAE":
     print(f'Saved {num_images * args.bs} generated images in the directory {save_dir}.')
 
 
+######### CVAE #########
+elif args.gen == "cvae" or args.gen == "CVAE":
+    from generators32.CCVAE import *
+    args.latent_size = 16 # CVAE
+    
+    fedgen = CCVAE(args).to(args.device)  # [transforms.ToTensor(),]
+    fedgen.load_state_dict(torch.load(args.gen_dir))
+    # summary(fedgen, x, c) # torchsummaryX
+    fedgen.eval()
+
+    # Create a directory to save the generated images
+    save_dir = args.img_dir + args.dataset + '/' + args.gen_dir[:-3] + '/'
+    os.makedirs(save_dir, exist_ok=True)
+
+    # Generate and save 60,000 images with random labels
+    num_images = len(train_data) // args.bs
+    print(f'Saving {num_images * args.bs} MNIST images in {save_dir}......')
+
+    with torch.no_grad():
+        for i in range(num_images):
+            generated_images, _ = fedgen.sample_image(args, sample_num=args.bs)
+            generated_images = generated_images.view(-1, args.output_channel, args.img_size, args.img_size)
+
+            # Save each image in the batch as a PNG file
+            for j in range(args.bs):
+                image = generated_images[j].squeeze(0)  # Reshape to 28x28
+                file_path = os.path.join(save_dir, f'FedVZE_image_{i * args.bs + j}.png')
+                if not os.path.exists(file_path):
+                    save_image(image, file_path, nrow=1)
+    print(f'Saved {num_images * args.bs} generated images in the directory {save_dir}.')
+
+
 ######### DDPM #########
 elif args.gen == "ddpm" or args.gen == "DDPM":
     from DDPM.ddpm28 import *
@@ -228,4 +260,6 @@ if args.gen == "ddpm" or args.gen == "DDPM":
 else:
     save_dir_2 = args.img_dir + args.dataset + '/' + args.gen_dir[:-3]
 
-os.system("python -m pytorch_fid " + save_dir_1 + ' ' + save_dir_2) #python -m pytorch_fid path/to/dataset1 path/to/dataset2
+# source activate devnet
+result = os.popen("python -m pytorch_fid " + save_dir_1 + ' ' + save_dir_2).read() #python -m pytorch_fid path/to/dataset1 path/to/dataset2
+print(result)
